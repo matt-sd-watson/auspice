94bb77d413be32e5b176c7f92e22f671421fbd96
Matthew Watson <matthew.watson@oahpp.ca>
2021-03-25 10:58:28 -0400

    preamble content to var with spacing

--- src/components/framework/footer.js
+++ src/components/framework/footer.js
@@ -132,11 +132,11 @@ export const getAcknowledgments = (metadata, dispatch) => {
     );
   }
 
-  const preambleContent = "This work is made possible by the open sharing of genetic data by research groups from all over the world. We gratefully acknowledge their contributions.\
-  Public Health Ontario maintains an internal subs-sampling strategy for the data displayed above. As such, any inferences that users may personally generate\
+  var preambleContent = "This work is made possible by the open sharing of genetic data by research groups from all over the world. We gratefully acknowledge their contributions. \
+  Public Health Ontario maintains an internal subs-sampling strategy for the data displayed above. As such, any inferences that users may personally generate \
   these data should not be viewed as absolute or conclusive. Public Health Ontario maintains the right to modify these data at any time."; 
   
-  const genericPreamble = (<div>{preambleContent}</div>);
+  var genericPreamble = (<div>{preambleContent}</div>);
 
   if (window.location.hostname === 'nextstrain.org') {
     return hardCodedFooters(dispatch, genericPreamble);
